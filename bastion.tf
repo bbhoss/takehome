98802c0ae5434bdeaf90aa6a4745a1edc049036f
Preston Marshall <preston.marshall@complymd.com>
2015-04-17 20:12:52 -0500

    Add SSH Proxying config for Ansible

--- bastion.tf
+++ bastion.tf
@@ -10,4 +10,15 @@ resource "aws_instance" "bastion" {
   subnet_id = "${aws_subnet.main.id}"
   security_groups = ["${aws_security_group.default.id}"]
   associate_public_ip_address = true
+
+  connection {
+    user = "centos"
+    key_file = "${var.key_path}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum -y install nc"
+    ]
+  }
 }
